bc5bb630e70cf1f705d7c3e3646b4bc68588f38f
Nurlan Seitov <seitov@gmail.com>
2024-05-06 02:06:25 +0600

    Initial Commit

---
+++ WholesaleStoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WholesaleStoreApp
+//
+//  Created by Nurlan Seitov on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
